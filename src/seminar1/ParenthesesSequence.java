922723fadd1b03b034137a1601dbd455eef45f83
veppev <vepppev5@gmail.com>
2017-10-10 02:57:29 +0300

    Solve ParenthesesSequence

--- src/seminar1/ParenthesesSequence.java
+++ src/seminar1/ParenthesesSequence.java
@@ -1,38 +1,43 @@
-//package seminar1;
-//
-//import java.io.BufferedReader;
-//import java.io.IOException;
-//import java.io.InputStreamReader;
-//
-///**
-// * 1. пустая строка — правильная скобочная последовательность;
-// * 2. правильная скобочная последовательность,
-// *      взятая в скобки одного типа — правильная скобочная последовательность;
-// * 3. правильная скобочная последовательность,
-// *      к которой приписана слева или справа правильная скобочная последовательность
-// *      — тоже правильная скобочная последовательность.
-// */
-//public class ParenthesesSequence {
-//
-//    private static final String QUIT = "q";
-//
-//    private static final char LEFT_PAREN     = '(';
-//    private static final char RIGHT_PAREN    = ')';
-//
-//    // sequence = "()()" | "((((" | ")()(" | ...
-//    private static boolean isBalanced(String sequence) {
-//        /* TODO: implement it */
-//        return false;
-//    }
-//
-//    public static void main(String[] args) {
-//        try (BufferedReader lineReader = new BufferedReader(new InputStreamReader(System.in))) {
-//            String sequence;
-//            while (!QUIT.equals(sequence = lineReader.readLine())) {
-//                System.out.println(isBalanced(sequence) ? "YES" : "NO");
-//            }
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//    }
-//}
+package seminar1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * 1. пустая строка — правильная скобочная последовательность;
+ * 2. правильная скобочная последовательность,
+ *      взятая в скобки одного типа — правильная скобочная последовательность;
+ * 3. правильная скобочная последовательность,
+ *      к которой приписана слева или справа правильная скобочная последовательность
+ *      — тоже правильная скобочная последовательность.
+ */
+public class ParenthesesSequence {
+
+    private static final String QUIT = "q";
+
+    private static final char LEFT_PAREN     = '(';
+    private static final char RIGHT_PAREN    = ')';
+
+    // sequence = "()()" | "((((" | ")()(" | ...
+    private static boolean isBalanced(String sequence) {
+        int depth = 0;
+        for (int i = 0; i < sequence.length(); i++)
+            switch (sequence.charAt(i)) {
+            case LEFT_PAREN : depth++; break;
+            case RIGHT_PAREN : depth--;
+            }
+        return depth == 0;
+    }
+
+    public static void main(String[] args) {
+        try (BufferedReader lineReader = new BufferedReader(new InputStreamReader(System.in))) {
+            String sequence;
+            while (!QUIT.equals(sequence = lineReader.readLine())) {
+                System.out.println(isBalanced(sequence) ? "YES" : "NO");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
