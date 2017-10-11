2e2a5e9ec0c1ee16a4cd77786f2308bcbf52ff73
veppev <vepppev5@gmail.com>
2017-10-11 06:12:54 +0300

    Realize task

--- src/seminar1/iterators/MergingIncreasingIterator.java
+++ src/seminar1/iterators/MergingIncreasingIterator.java
@@ -7,30 +7,49 @@ import java.util.Iterator;
  * first = 1,3,4,5,7
  * second = 0,2,4,6,8
  * result = 0,1,2,3,4,4,5,6,7,8
- *
+ * <p>
  * Time = O(k),
- *  k — суммарное количество элементов
+ * k — суммарное количество элементов
  */
 public class MergingIncreasingIterator implements Iterator<Integer> {
 
     private IncreasingIterator first;
     private IncreasingIterator second;
+    private Integer firstNumber, secondNumber;
 
     public MergingIncreasingIterator(IncreasingIterator first, IncreasingIterator second) {
         this.first = first;
         this.second = second;
-        /* TODO: implement it */
+//        if (first.hasNext()) {
+//            firstNumber = first.next();
+//        }
+//        if (second.hasNext()) {
+//            secondNumber = second.next();
+//        }
     }
 
     @Override
     public boolean hasNext() {
-        /* TODO: implement it */
-        return false;
+        return first.hasNext() || second.hasNext();
     }
 
     @Override
     public Integer next() {
-        /* TODO: implement it */
-        return null;
+        if (firstNumber == null && first.hasNext()) firstNumber = first.next();
+        if (secondNumber == null && second.hasNext()) secondNumber = second.next();
+
+        if (firstNumber == null) return second.next();
+        if (secondNumber == null) return first.next();
+
+        int result;
+
+        if (firstNumber < secondNumber) {
+            result = firstNumber;
+            firstNumber = null;
+        } else {
+            result = secondNumber;
+            secondNumber = null;
+        }
+        return result;
     }
 }
